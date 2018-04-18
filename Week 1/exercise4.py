d8b216234aa166418e3c74d8ef4bf76d78fa2a4e
lizneth72 <lizneth72@yahoo.com>
2018-04-18 16:13:47 -0400

    Added exercise task

--- Week 1/exercise4.py
+++ Week 1/exercise4.py
@@ -1,3 +1,20 @@
+'''
+4. Create a show_version variable that contains the following
+
+ show_version = "*0        CISCO881-SEC-K9       FTX0000038X    " 
+
+
+Remove all leading and trailing whitespace from the string.
+
+Split the string and extract the model and serial_number from it.
+
+Check if 'Cisco' is contained in the model string (ignore capitalization).
+
+Check if '881' is in the model string.
+
+Print out both the serial number and the model.
+'''
+
 from __future__ import print_function
 from __future__ import unicode_literals
 
